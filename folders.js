add63c0c57f35cba9d6468dd29e2136fd3d9bf61
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-06-28 22:38:08 -0300

    valida se existe um arquivo e cria, caso não exista

--- folders.js
+++ folders.js
@@ -1,10 +1,9 @@
 /*
     1- Criar uma pasta/diretório -> Recursivamente com validação
     2- Remover um diretório vazio
-    4- Criar Arquivo
-    5- Validar se o arquivo já existe antes de criar
-    6- Remover um arquivo do diretório
-    7- Remover um diretório e todos os seus arquivos filho (subpastas e arquivos)
+    3- Validar se arquivo existe no diretório e criar, caso não exista
+    4- Remover um arquivo do diretório
+    5- Remover um diretório e todos os seus arquivos filho (subpastas e arquivos)
  */
 
 const fs = require('fs');
@@ -33,4 +32,38 @@ if(!fs.existsSync(fullDir)) {
     
 }
 
-//2- Remover um diretório vazio
+// 2- Remover um diretório vazio ou com subdiretórios
+if(fs.lstatSync(fullDir).isDirectory()) {
+    fs.readdir(fullDir, (err, files) => {
+        if(err) throw err;
+    
+        if (files.length > 0) {
+            console.log(`O diretório ${fullDir} possui arquivos e não será excluído.`)
+        } else {
+            console.log(`O diretório ${fullDir} não possui arquivos e será excluído.`)
+            fs.rmdirSync(fullDir)
+        }
+    })
+}
+
+//3- Validar se arquivo existe no diretório e criar caso não exista
+let meuPrimeiroArquivo = path.join(__dirname, 'primeiroArquivo.txt')
+fs.access(meuPrimeiroArquivo, fs.constants.F_OK, (err) => {
+    if(err) {
+        console.log('O arquivo não existe e será criado!')
+        fs.writeFileSync(meuPrimeiroArquivo, '', (err) => {
+            if(err) throw err;
+        });
+    } else {
+        console.log('O arquivo já existe e não será criado!')
+    }
+})
+
+//5- Remover um diretório e todos os seus arquivos filho (subpastas e arquivos)
+fs.rm(fullDir, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Pasta removida com sucesso.');
+});
